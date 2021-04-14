32256a6919d718229cb39fb85fad01a2f869959f
J-R4 <JoshuaFanera@gmail.com>
2021-04-14 23:25:13 +0700

    trying using atlas

--- server/services/series/config/mongodb.js
+++ server/services/series/config/mongodb.js
@@ -3,7 +3,8 @@ const { MongoClient } = require('mongodb')
 let database = null
 
 const connectMongoDB = (cb) => {
-  const uri = 'mongodb://localhost:27017'
+  // const uri = 'mongodb://localhost:27017'
+  const uri = 'mongodb+srv://admin:admin@cluster0.cucg4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
