8f05dec2bce78e6c78a4396d77a34f1b8e6891bc
J-R4 <JoshuaFanera@gmail.com>
2021-04-16 14:18:01 +0700

    changing the ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,33 +1,35 @@
+/** @format */
+
 module.exports = {
-  apps: [
-    {
-      name: "entertainme - Client",
-      script: "cd client/app && npm start",
-    },
-    {
-      name: "entertainme - Orchestrator",
-      script: "cd server/orchestrator && npm install &&  nodemon index.js",
-      env: {
-        PORT: 4000,
-      },
-    },
-    {
-      name: "entertainme - Service Movies",
-      script: "cd server/services/movies && npm install && nodemon app.js",
-      env: {
-        DATABASE_NAME: "entertainme",
-        COLLECTION_NAME: "movies",
-        PORT: 4001,
-      },
-    },
-    {
-      name: "entertainme - Service TV Series",
-      script: "cd server/services/series && npm install && nodemon app.js",
-      env: {
-        DATABASE_NAME: "entertainme",
-        COLLECTION_NAME: "series",
-        PORT: 4002,
-      },
-    },
-  ],
-}
+    apps: [
+        {
+            name: 'entertainme - Client',
+            script: 'cd client/app && npm start',
+        },
+        {
+            name: 'entertainme - Orchestrator',
+            script: 'cd server/orchestrator && npm install &&  node index.js',
+            env: {
+                PORT: 4000,
+            },
+        },
+        {
+            name: 'entertainme - Service Movies',
+            script: 'cd server/services/movies && npm install && node app.js',
+            env: {
+                DATABASE_NAME: 'entertainme',
+                COLLECTION_NAME: 'movies',
+                PORT: 4001,
+            },
+        },
+        {
+            name: 'entertainme - Service TV Series',
+            script: 'cd server/services/series && npm install && node app.js',
+            env: {
+                DATABASE_NAME: 'entertainme',
+                COLLECTION_NAME: 'series',
+                PORT: 4002,
+            },
+        },
+    ],
+};
